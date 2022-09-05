e13cce102bad39ca10893d4ce2bd058698175971
Patrick Rios <angleopatrickriosnolasco@gmail.com>
2022-09-05 17:32:01 -0500

    feat: exercise 2

---
+++ profile.js
@@ -0,0 +1,10 @@
+let name = "Patrick";
+let age = 19;
+let dev = true;
+let birthday = new Date(2003, 4, 9);
+let book = {
+  title: "La esfera mágica",
+  author: "Eli Echevarria",
+  date: new Date(2015, 5, 22),
+  link: null,
+};
