fb75672bcf578e12e2e16b7453286eea1fb87025
Patrick Rios <angleopatrickriosnolasco@gmail.com>
2022-09-26 09:26:53 -0500

    leccion 1883

---
+++ ejercicios/leccion1883.js
@@ -0,0 +1,10 @@
+// - Abre una nueva ventana "about:blank" en Google Chrome
+
+// - Abre la consola de desarrollador de Google Chrome (F12)
+
+// - Pregunta al usuario cuál es su edad y almacénala en una variable llamada edad
+
+// desarrollo
+// solo tenemos que poner en la barra de busqueda about:blank
+// pulsar F12
+// en la consola const name = promp("cual es tu nombre"), const edad = promp("Cual es tu edad")
