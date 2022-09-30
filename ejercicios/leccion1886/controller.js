45111fa623025802d82c0b8c7829015f84c3a62f
Patrick Rios <angleopatrickriosnolasco@gmail.com>
2022-09-30 06:53:38 -0500

    leecion 1886

---
+++ ejercicios/leccion1886/controller.js
@@ -0,0 +1,8 @@
+const sumar = (a, b) => {
+  return a + b;
+};
+const multi = (a, b) => {
+  return a * b;
+};
+
+export { sumar, multi };
---
+++ ejercicios/leccion1886/index.js
@@ -0,0 +1,5 @@
+import { sumar, multi } from "./controller.js";
+import chalk from "chalk";
+
+console.log(sumar(1, 2));
+console.log(chalk.green(multi(2, 6)));
