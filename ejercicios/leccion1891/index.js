b4dbdbdbc24e0222129c5cfc46268a6446eff677
Patrick Rios <angleopatrickriosnolasco@gmail.com>
2022-10-03 18:49:48 -0500

    exercise 1890 1891

---
+++ ejercicios/leccion1891/index.js
@@ -0,0 +1,6 @@
+const boton = document.getElementById("boton");
+boton.addEventListener("click", () => alert("click en el boton"));
+
+$("#botonQuery").click(function () {
+  alert("Hola, estoy utilizando jQuery");
+});
