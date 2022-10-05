27a5cf70d0e265870a45490313d3351a99ee19f3
Patrick Rios <angleopatrickriosnolasco@gmail.com>
2022-10-04 21:46:17 -0500

    resolution leccion1894

--- ejercicios/leccion1894/index.js
+++ ejercicios/leccion1894/index.js
@@ -1,38 +1,44 @@
+let markers, map;
+
 function initMap() {
   const posicion = {
     lat: -25.363,
     lng: 131.044,
   };
-  const map = new google.maps.Map({
+
+  map = new google.maps.Map(document.getElementById("map"), {
     zoom: 4,
     center: posicion,
   });
-  const marker = new google.maps.Map({
-    position: posicion,
-    map,
-    title: "Posición inicial",
-  });
-}
 
-function geoPosiciona() {
-  if (navigator.geolocation) {
-    const geoLoc = navigator.geolocation;
-    const options = { timeout: 60000 };
-    const watchPos = geoLoc.watchPosition(centraMapa, onError, options);
-  } else {
-    alert("tu navegador no admite geoLocalización");
-  }
-}
-
-function centraMapa(position) {
-  const nuevaPos = {
-    lat: position.coords.latitude,
-    lng: position.coords.longitude,
-  };
-  console.log(nuevaPos)
-  marker.setPosition(nuevaPos)
-  map.setCenter(nuevaPos)
-}
-function onError(error) {
-  console.error(error);
+  markers.push(
+    new google.maps.Marker({
+      position: {
+        lat: 43.288773445332524,
+        lng: -2.163845628766325,
+      },
+      map,
+      title: "Lima",
+    })
+  );
+  markers.push(
+    new google.maps.Marker({
+      position: {
+        lat: 36.164983935422484,
+        lng: -86.78260332112534,
+      },
+      map,
+      title: "Huanuco",
+    })
+  );
+  markers.push(
+    new google.maps.Marker({
+      position: {
+        lat: 36.242150706885596,
+        lng: -6.0775131009957715,
+      },
+      map,
+      title: "Trujillo",
+    })
+  );
 }
